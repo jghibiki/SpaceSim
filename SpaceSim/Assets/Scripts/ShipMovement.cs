3e430b43e538ba23b58442d52d3f561a2f378d00
jghibiki <jghibiki.games@gmail.com>
2018-03-28 17:06:09 -0500

    Finally gets movement working

---
+++ SpaceSim/Assets/Scripts/ShipMovement.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour {
+
+
+	public float throttle = 0f;
+	
+	public float forward_thrust = 2f;
+	public float backward_thrust = 1.5f;
+	public float acceleration = 1.5f;
+
+	public float pitch_yaw_thrust = 1f;
+	public float roll_thrust = 1f;
+
+	public bool invert_y = true;
+
+	public bool use_exterior_camera = false;
+
+
+	private SoftwareCursor swc;
+	private Rigidbody rb;
+	private Camera interior_camera;
+	private Camera exterior_camera;
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+
+		swc = GameObject.Find("SoftwareCursor").GetComponent<SoftwareCursor>();
+
+		interior_camera = Camera.main;
+		interior_camera.enabled = true;
+
+		exterior_camera = GameObject.Find("ExteriorCamera").GetComponent<Camera>();
+		exterior_camera.enabled = false;
+
+		rb.centerOfMass = interior_camera.transform.position;
+	}
+
+	void Update(){
+
+		if(Input.GetKeyUp("=")){
+			SwapCamera();
+		}
+
+		if(Input.GetKey("w")){
+			throttle += 0.01f;
+			throttle = Mathf.Clamp(throttle, -1, 1);
+		}	
+		else if(Input.GetKey("s")){
+			throttle -= 0.01f;
+			throttle = Mathf.Clamp(throttle, -1, 1);
+		}	
+		else if(Input.GetKey("x")){
+			throttle = 0f;
+		}
+
+		if(throttle > 0){
+			rb.AddRelativeForce( Vector3.forward * throttle * forward_thrust);
+			rb.velocity = Vector3.ClampMagnitude(rb.velocity, forward_thrust);
+			rb.drag = 0f;
+		}
+		else if (throttle < 0){
+			rb.AddRelativeForce(-Vector3.forward * throttle * backward_thrust);
+			rb.velocity = Vector3.ClampMagnitude(rb.velocity, backward_thrust);
+			rb.drag = 0f;
+		}
+		else{
+			rb.drag = 1f;
+		}
+
+		var dampen_rotation = false;
+		if(swc.cursor_magnitude.magnitude > 0.05){
+			var cursor_magnitude = swc.cursor_magnitude;
+			Debug.Log(cursor_magnitude);
+			
+			if (invert_y){
+				cursor_magnitude.y *= -1;
+			}
+
+			var torque_vector = new Vector3(cursor_magnitude.y, cursor_magnitude.x, 0f);
+
+			rb.AddRelativeTorque(torque_vector * pitch_yaw_thrust);
+		}
+		else{
+			dampen_rotation = true;
+		}
+
+
+		if(Input.GetKey("q")){
+			rb.AddRelativeTorque(Vector3.forward * pitch_yaw_thrust);
+		}
+		else if(Input.GetKey("e")){
+			rb.AddRelativeTorque(-Vector3.forward * pitch_yaw_thrust);
+		}
+		else{
+			dampen_rotation = true;
+		}
+
+		if(dampen_rotation){
+			rb.angularDrag = 5f;
+		}
+		else{
+			rb.angularDrag = 1f;
+		}
+
+	}
+	
+
+	void SwapCamera(){
+		if(!use_exterior_camera){
+			use_exterior_camera = true;
+			interior_camera.enabled = false;
+			exterior_camera.enabled = true;
+		}
+		else{
+			use_exterior_camera = false;
+			interior_camera.enabled = true;
+			exterior_camera.enabled = false;
+		}
+	}
+ 
+       
+}
---
+++ SpaceSim/Assets/Scripts/SoftwareCursor.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoftwareCursor : MonoBehaviour {
+
+	public float mouse_radius;
+	public float deadzone;
+
+	public Vector3 relative_mouse_position = new Vector3();
+	public Vector3 cursor_magnitude = new Vector3();
+
+	private Vector3 software_mouse_position = new Vector3();
+
+
+	// Use this for initialization
+	void Start () {
+		Cursor.lockState = CursorLockMode.Confined;			
+		Cursor.visible = false;
+
+		var software_mouse_position = FindCenterScreen();
+		var offset = FindDistanceFromCenterOfScreen();
+		software_mouse_position += offset;
+
+		relative_mouse_position = ConstrictReletiveMousePosition(offset);
+		cursor_magnitude = GetCursorMagnitude(relative_mouse_position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		var center_screen = FindCenterScreen();
+		var offset = FindDistanceFromCenterOfScreen();
+		software_mouse_position = center_screen + offset;
+
+		transform.position = software_mouse_position;
+
+		relative_mouse_position = ConstrictReletiveMousePosition(offset);
+		cursor_magnitude = GetCursorMagnitude(relative_mouse_position);
+	}
+
+	private Vector3 FindCenterScreen(){
+		var mouse_pos = Input.mousePosition;
+
+		var center_screen_x = Mathf.Floor(Screen.width/2.0f);
+		var center_screen_y = Mathf.Floor(Screen.height/2.0f);
+		var center_screen = new Vector3(center_screen_x, center_screen_y, 0);
+
+		return center_screen;
+	}
+
+	private Vector3 FindDistanceFromCenterOfScreen(){
+		var resolution = Screen.currentResolution;
+		var mouse_pos = Input.mousePosition;
+
+		float mouse_x_percent = Mathf.Clamp(mouse_pos.x/Screen.width, 0f, 1f);
+		float mouse_y_percent = Mathf.Clamp(mouse_pos.y/Screen.height, 0f, 1f);
+
+
+		var mouse_x = (Screen.width *0.5f) - (Screen.width * mouse_x_percent);
+		var mouse_y = (Screen.height *0.5f) - (Screen.height * mouse_y_percent);
+
+		var scaled_mouse_pos = - new Vector3(mouse_x, mouse_y, 0f);
+		scaled_mouse_pos = Vector3.ClampMagnitude(scaled_mouse_pos, mouse_radius * Screen.height);
+
+		return scaled_mouse_pos;
+	}
+
+	private Vector3 GetCursorMagnitude(Vector3 offset){
+		var percent_magnitude_x = (offset.x / Screen.height) / mouse_radius;
+		var percent_magnitude_y = (offset.y / Screen.height) / mouse_radius;
+
+		var percent_magnitude = new Vector3(percent_magnitude_x, percent_magnitude_y, 0);
+
+
+		return Vector3.ClampMagnitude(percent_magnitude, mouse_radius * Screen.height);
+	}
+
+	private Vector3 ConstrictReletiveMousePosition(Vector3 offset){
+		/* 
+		var percent_magnitude = offset.magnitude / Screen.height;
+		if(percent_magnitude < deadzone){
+			return Vector3.zero;
+		}
+
+		//rescale percentage starting at deadzone;
+		var diff = (Screen.height * deadzone);
+
+		var y_percent = (offset.y + diff) / Screen.height;
+		var x_percent = (offset.x + diff) / Screen.height;
+
+		if(y_percent > 0f && Mathf.Abs(x_percent) < 0.05f){
+			offset.x = 0f;
+		}
+
+		if(y_percent < 0f && Mathf.Abs(x_percent) < 0.05f){
+			offset.x = 0f;
+		}
+
+		if(x_percent < 0f && Mathf.Abs(y_percent) < 0.05f){
+			offset.y = 0f;
+		}
+
+		if(x_percent > 0f && Mathf.Abs(y_percent) < 0.05f){
+			offset.y = 0f;
+		}
+
+*/
+
+		return offset;
+	}
+
+}
